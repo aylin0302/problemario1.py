da7d76a5ce8ec985d32fdf2ede4990620a0bbcf8
aylin0302 <aylinroa89@gmail.com>
2024-11-27 19:37:48 -0600

    Create problema3.py

---
+++ problema3.py
@@ -0,0 +1,8 @@
+#Ingresar la nota del examen de programación y si es mayor o igual a siete imprimir
+#que aprobó, sino que debe recuperar.
+
+nota = int(input("ingrese nota"))
+if nota >= 70: 
+    print("aprobo")
+elif nota <70: 
+    print("no aprobo")
